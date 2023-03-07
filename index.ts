471e6259a2f007e07274d5acf5b57c7227123e71
seungryeol <opop0421@naver.com>
2023-03-07 18:07:26 +0900

    s3 로그 저장

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ const app = express();
 import detenv from 'dotenv';
 detenv.config();
 import morgan from 'morgan';
-
+import  logger,{LoggerStream} from './config/logger';
 
 
 app.set('port',  process.env.DEV_PORT || 3030);
 app.use(express.json());
-app.use(morgan('combined'));
-
+//app.use(morgan('combined'));
+app.use(morgan('combined', { stream: new LoggerStream() }));
 
 app.use('/api', require('./routes/popup-data'));
  
@@ -28,5 +28,7 @@ app.use((req, res, next) => {
 
 app.listen(process.env.DEV_PORT , () => {  
     console.log(app.get('port'), '번 포트에서 대기 중!');
-
+    logger.info(
+        `Server On : http://${process.env.HOST}:${process.env.DEV_PORT}/`,
+      );
 });
